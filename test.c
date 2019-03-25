467d7c3949461a40fe9345acf8da32cffcc401d8
yuanjiajun <768747811@qq.com>
2019-03-25 16:24:05 +0800

    pi

--- test.c
+++ test.c
@@ -1,16 +1,33 @@
+// #include <stdio.h>
+// typedef unsigned char uint8;
+// int main(void)
+// {
+// // uint8 txt = 0x45;
+// // uint8 *p = &txt;
+// struct test
+// {
+//     //uint8  version:4,hdr_len:4;
+//     uint8  a;
+// };
+// // struct test *txt_demo = (struct test *)p;
+// // printf("version=%d",txt_demo->version);
+// // printf("hdr_len=%d\n",txt_demo->hdr_len);
+// // printf("+++++\n");
+
+// struct test b ={.a}
+
+// return 0;
+// }
+
 #include <stdio.h>
-typedef unsigned char uint8;
-int main(void)
+#include <math.h>
+
+int main()
 {
-uint8 txt = 0x45;
-uint8 *p = &txt;
-struct test
-{
-    uint8  version:4,hdr_len:4;
-};
-struct test *txt_demo = (struct test *)p;
-printf("version=%d",txt_demo->version);
-printf("hdr_len=%d\n",txt_demo->hdr_len);
-printf("+++++\n");
-return 0;
+   char str[80];
+
+   sprintf(str, "Pi 的值 = %f", M_PI);
+   puts(str);
+   
+   return(0);
 }
