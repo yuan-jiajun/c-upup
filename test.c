b1c6beb98cdff544428056a9fccf2f2f993a6ae3
yuanjiajun <768747811@qq.com>
2019-03-28 20:46:07 +0800

    strtoul

--- test.c
+++ test.c
@@ -1,33 +1,15 @@
-// #include <stdio.h>
-// typedef unsigned char uint8;
-// int main(void)
-// {
-// // uint8 txt = 0x45;
-// // uint8 *p = &txt;
-// struct test
-// {
-//     //uint8  version:4,hdr_len:4;
-//     uint8  a;
-// };
-// // struct test *txt_demo = (struct test *)p;
-// // printf("version=%d",txt_demo->version);
-// // printf("hdr_len=%d\n",txt_demo->hdr_len);
-// // printf("+++++\n");
-
-// struct test b ={.a}
-
-// return 0;
-// }
-
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 int main()
 {
-   char str[80];
+   char str[30] = "2030300 This is test";
+   char *ptr;
+   long ret;
 
-   sprintf(str, "Pi 的值 = %f", M_PI);
-   puts(str);
-   
+   ret = strtoul(str, &ptr, 10);
+   printf("数字（无符号长整数）是 %lu\n", ret);
+   printf("字符串部分是 |%s|", ptr);
+
    return(0);
 }
