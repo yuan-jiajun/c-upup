6e6779f76ef5214cdfa38cc0e2ec8ccd9e4a7afd
yuanjiajun <768747811@qq.com>
2019-04-22 15:55:32 +0800

    abcdef

--- length.c
+++ length.c
@@ -6,8 +6,9 @@
 #include <string.h>
 int main()
 {
-    char *ring_write = "ring_for_write";
+    char *ring_write = "abcdef";
     int length = strlen(ring_write);
     printf("length=%d",length);
+    printf("\n");
     return 0;
 }
