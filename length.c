e29e146f3ce7cb708a0c88659c99b88a72f02f41
yuanjiajun <768747811@qq.com>
2019-04-22 15:53:40 +0800

    start.sh

--- length.c
+++ length.c
@@ -3,10 +3,11 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 int main()
 {
     char *ring_write = "ring_for_write";
     int length = strlen(ring_write);
-    printf(length=%d,length);
-    return(0);
+    printf("length=%d",length);
+    return 0;
 }
