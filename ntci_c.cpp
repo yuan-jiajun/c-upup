ccc5f67459eba711819e096fe60b949d1575536d
yuanjiajun <768747811@qq.com>
2019-04-22 15:21:00 +0800

    length

---
+++ ntci_c.cpp
@@ -0,0 +1,12 @@
+//
+// Created by yuanjiajun on 2019/4/22.
+//
+
+#include <stdio.h>
+int main()
+{
+    char *ring_write = "ring_for_write";
+    int length = strlen(ring_write);
+    printf(length=%d,length);
+    return(0);
+}
