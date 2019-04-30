b6f08e29cd337fe635ad6da949e22b2ad673b49b
yuanjiajun <768747811@qq.com>
2019-04-30 16:23:39 +0800

    abcdef

--- struct.c
+++ struct.c
@@ -2,13 +2,12 @@
 // Created by yuanjiajun on 2019/4/30.
 //
 
-
+struct TCP_HEADER {
+    unsigned int fin:1, syn:1, rst:1, psh:1, ack:1, urg:1, ecn_echo:1, cwr:1;
+};
 
 int main()
 {
-    struct TCP_HEADER {
-        unsigned int fin:1, syn:1, rst:1, psh:1, ack:1, urg:1, ecn_echo:1, cwr:1;
-    };
 
     struct TCP_HEADER tcp_header = 214;
 
@@ -16,6 +15,6 @@ int main()
                    ((unsigned int)tcp_header.ack<<4) + ((unsigned int)tcp_header.psh<<3) + ((unsigned int)tcp_header.rst<<2) +
                    ((unsigned int)tcp_header.syn<<1) + ((unsigned int)tcp_header.fin);
 
-    printf("flags = %d",flags);
+    printf("flags = %d\n",flags);
     return(0);
 }
