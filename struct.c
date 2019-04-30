b3dc4e23741afccf62930b07591ca8a71a719db2
yuanjiajun <768747811@qq.com>
2019-04-30 16:20:04 +0800

    abcdef

--- struct.c
+++ struct.c
@@ -10,7 +10,7 @@ int main()
         unsigned int fin:1, syn:1, rst:1, psh:1, ack:1, urg:1, ecn_echo:1, cwr:1;
     };
 
-    TCP_HEADER tcp_header = 214;
+    struct TCP_HEADER tcp_header = 214;
 
     unsigned int flags =((uint8_t)tcp_header.cwr<<7) + ((uint8_t)tcp_header.ecn_echo<<6) + ((uint8_t)tcp_header.urg<<5) +
                    ((uint8_t)tcp_header.ack<<4) + ((uint8_t)tcp_header.psh<<3) + ((uint8_t)tcp_header.rst<<2) +
