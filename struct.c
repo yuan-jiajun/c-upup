037c436df31e3b2bd583c2e0b522ce01b4afad0d
yuanjiajun <768747811@qq.com>
2019-04-30 16:18:42 +0800

    abcdef

--- struct.c
+++ struct.c
@@ -12,9 +12,9 @@ int main()
 
     TCP_HEADER tcp_header = 214;
 
-    unsigned int flags =((uint8_t)pkt->ip_route.tcp.cwr<<7) + ((uint8_t)pkt->ip_route.tcp.ecn_echo<<6) + ((uint8_t)pkt->ip_route.tcp.urg<<5) +
-                   ((uint8_t)pkt->ip_route.tcp.ack<<4) + ((uint8_t)pkt->ip_route.tcp.psh<<3) + ((uint8_t)pkt->ip_route.tcp.rst<<2) +
-                   ((uint8_t)pkt->ip_route.tcp.syn<<1) + ((uint8_t)pkt->ip_route.tcp.fin);
+    unsigned int flags =((uint8_t)tcp_header.cwr<<7) + ((uint8_t)tcp_header.ecn_echo<<6) + ((uint8_t)tcp_header.urg<<5) +
+                   ((uint8_t)tcp_header.ack<<4) + ((uint8_t)tcp_header.psh<<3) + ((uint8_t)tcp_header.rst<<2) +
+                   ((uint8_t)tcp_header.syn<<1) + ((uint8_t)tcp_header.fin);
 
     printf("flags = %d",flags);
     return(0);
