40e056d8602a97ab6b1302746cd220468a5eea43
yuanjiajun <768747811@qq.com>
2019-05-15 15:30:19 +0800

    add folder src

---
+++ cmake-build-debug/CMakeFiles/c_test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/f/workspace/c/c_test/src/develop.c" "/cygdrive/f/workspace/c/c_test/cmake-build-debug/CMakeFiles/c_test.dir/src/develop.c.o"
+  "/cygdrive/f/workspace/c/c_test/src/length.c" "/cygdrive/f/workspace/c/c_test/cmake-build-debug/CMakeFiles/c_test.dir/src/length.c.o"
+  "/cygdrive/f/workspace/c/c_test/src/struct.c" "/cygdrive/f/workspace/c/c_test/cmake-build-debug/CMakeFiles/c_test.dir/src/struct.c.o"
+  "/cygdrive/f/workspace/c/c_test/src/test.c" "/cygdrive/f/workspace/c/c_test/cmake-build-debug/CMakeFiles/c_test.dir/src/test.c.o"
+  "/cygdrive/f/workspace/c/c_test/src/vcs.c" "/cygdrive/f/workspace/c/c_test/cmake-build-debug/CMakeFiles/c_test.dir/src/vcs.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/c_test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_test.dir/src/develop.c.o"
+  "CMakeFiles/c_test.dir/src/length.c.o"
+  "CMakeFiles/c_test.dir/src/struct.c.o"
+  "CMakeFiles/c_test.dir/src/test.c.o"
+  "CMakeFiles/c_test.dir/src/vcs.c.o"
+  "c_test.pdb"
+  "c_test.exe"
+  "libc_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
