742d2ada2bb572d936790a7a086efa78ac55bf9c
yuanjiajun <768747811@qq.com>
2019-09-17 10:52:59 +0800

    free test

--- tests/free.c
+++ tests/free.c
@@ -5,11 +5,16 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-#define SAFE_RELEASE(p) { if(p){ free(p);  p=NULL;}}
+void SAFE_RELEASE(void *p) {
+    if (p) {
+        free(p);
+        p = NULL;
+    }
+}
 
 int main() {
-    char *p ="q";
+    char *q = "q";
     printf("create a branch named develop2.\n");
-    SAFE_RELEASE(p)
-    SAFE_RELEASE(p)
+    SAFE_RELEASE(q);
+    SAFE_RELEASE(q);
 }
